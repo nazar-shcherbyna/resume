a431089d361d5fdc2b0d8b58070489d617f3f716
Nazar Shcherbyna <shherbina.nazar@gmail.com>
2025-09-12 17:20:13 -0700

    refactor: add type definition for metadata and include favicon in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import { Metadata } from "next";
 import "../styles/globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Nazar Shcherbyna Resume",
   description: "Minimalistic resume website for Nazar Shcherbyna",
 };
@@ -12,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body className="bg-white dark:bg-neutral-950">{children}</body>
     </html>
   );
